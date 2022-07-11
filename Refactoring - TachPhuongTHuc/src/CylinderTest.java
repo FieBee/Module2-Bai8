fb52d6bb5c1fd0950f2967c48b0c5534277536b9
FieBee <phuonglf98@gmail.com>
2022-07-11 22:39:18 +0700

    add Refactoring - TachPhuongThuc

---
+++ Refactoring - TachPhuongTHuc/src/CylinderTest.java
@@ -0,0 +1,11 @@
+
+public class CylinderTest extends Cylinder{
+    public static void main(String[] args) {
+//        Cylinder cylinder = new Cylinder();
+
+        System.out.println(getVolume(25,6));
+        System.out.println(getBaseArea(10));
+        System.out.println(getPerimeter(10));
+
+    }
+}
