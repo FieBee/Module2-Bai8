70f2bc88249e7684b69c651b10e08be319028bc1
FieBee <phuonglf98@gmail.com>
2022-07-11 18:27:39 +0700

    add TH-Calculator

---
+++ Calculator/src/CalculatorTest.java
@@ -0,0 +1,7 @@
+public class CalculatorTest extends Calculator{
+    public static void main(String[] args) {
+        Calculator calculator1 = new Calculator();
+        int num1 = calculate(100, 20,ADDITION);
+        System.out.println(num1);
+    }
+}
